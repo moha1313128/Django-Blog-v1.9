d61c3061af4ddeba2a43456241e07047ba41d8b2
moha1313128 <moha1313128@gmail.com>
2020-02-08 23:00:41 +0100

    Search Post

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,6 +4,13 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save 
 from django.utils.text import slugify
+from django.utils import timezone
+
+
+class PostManager(models.Manager):
+	def active(self, *args, **kwargs):
+		# Post.objects.all() = super(PostManger, self).all()
+		return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 def upload_location(instance, filename):
 	return "%s/%s" %(instance.id, filename)
@@ -12,19 +19,23 @@ class Post(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
+	# image = StdImageField(upload_to=upload_location,  size=(200, 200))
 	image = models.ImageField(
 		null=True, 
 		blank=True, 
+		editable=True,
 		upload_to=upload_location, 
 		width_field="width_field", 
 		height_field="height_field")
-	height_field = models.IntegerField(default=0)
-	width_field = models.IntegerField(default=0)	
+	height_field = models.PositiveIntegerField(default="200")
+	width_field = models.PositiveIntegerField(default="200")	
 	content = models.TextField()
 	draft = models.BooleanField(default=False)
 	publish = models.DateField(auto_now=False, auto_now_add=False)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	objects = PostManager()
 
 	def __unicode__(self):
 		return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,17 +1,19 @@
 from urllib.parse import urlencode, quote_plus
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from django.utils import timezone
 from django.contrib import messages
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-	# if not request.user.is_staff or not request.user.is_superuser:
-	# 	raise Http404
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 
-	# if not request.user.is_auhenticated:
-	# 	raise Http404
+	if not request.user.is_auhenticated:
+		raise Http404
 
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
@@ -27,8 +29,8 @@ def post_create(request):
 	return render(request, "post_form.html", context)
 
 def post_update(request, slug=None):
-	# if not request.user.is_staff or not request.user.is_superuser:
-	# 	raise Http404
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
@@ -47,6 +49,9 @@ def post_update(request, slug=None):
 
 def post_detail(request, slug):
 	instance = get_object_or_404(Post, slug=slug)
+	if instance.publish > timezone.now().date() or instance.draft:
+		if not request.user.is_staff or not request.user.is_superuser:
+			raise Http404
 	share_string = quote_plus(instance.content)
 	context = {
 		"title": instance.title,
@@ -56,7 +61,19 @@ def post_detail(request, slug):
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	today = timezone.now().date()
+	queryset_list = Post.objects.active()  #filter(publish__lte=timezone.now())#active() #filter(draft=False)#.order_by("-timestamp")
+	if request.user.is_staff or not request.user.is_superuser:
+		queryset_list = Post.objects.all()
+
+	query = request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(
+			Q(title__icontains=query) |
+			Q(content__icontains=query) |
+			Q(user__first_name__icontains=query) |
+			Q(user__last_name__icontains=query) 
+			).distinct()
 	paginator = Paginator(queryset_list, 3)
 	page_request_var = "page"
 	page = request.GET.get(page_request_var)
@@ -75,8 +92,8 @@ def post_list(request):
 	return render(request, "post_list.html", context)
 
 def post_delete(request, slug=None):
-	# if not request.user.is_staff or not request.user.is_superuser:
-	# 	raise Http404
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully Deleted")
